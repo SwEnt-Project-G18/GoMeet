00a7da25a8a6755ad7aeb7db0e6284b2bffb5cfc
Antoine Garin <antoine.garin@epfl.ch>
2024-03-27 22:44:38 +0100

    setup the API KEY for the CI setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,22 @@ plugins {
     id("com.ncorti.ktfmt.gradle") version "0.16.0"
     id("kotlin-android")
     id("org.sonarqube") version "4.4.1.3373"
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+}
+
+secrets {
+    // Optionally specify a different file name containing your secrets.
+    // The plugin defaults to "local.properties"
+    propertiesFileName = "secrets.properties"
+
+    // A properties file containing default secret values. This file can be
+    // checked in version control.
+    defaultPropertiesFileName = "local.defaults.properties"
+
+    // Configure which keys should be ignored by the plugin by providing regular expressions.
+    // "sdk.dir" is ignored by default.
+    ignoreList.add("keyToIgnore") // Ignore the key "keyToIgnore"
+    ignoreList.add("sdk.*")       // Ignore all keys matching the regexp "sdk.*"
 }
 
 
