870174da5307535f01e79be5d55ed534aca1f7d8
chorfi <chorfiyouss@gmail.com>
2024-04-02 20:44:57 +0200

    WelcomeScreen update

--- app/src/main/java/com/github/se/gomeet/MainActivity.kt
+++ app/src/main/java/com/github/se/gomeet/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -30,6 +33,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -39,12 +43,14 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.github.se.gomeet.ui.WelcomeScreen
 import com.github.se.gomeet.ui.create.Create
 import com.github.se.gomeet.ui.events.Events
 import com.github.se.gomeet.ui.explore.Explore
 import com.github.se.gomeet.ui.navigation.NavigationActions
 import com.github.se.gomeet.ui.navigation.Route
 import com.github.se.gomeet.ui.profile.Profile
+import com.github.se.gomeet.ui.theme.DarkCyan
 import com.github.se.gomeet.ui.theme.GoMeetTheme
 import com.github.se.gomeet.ui.trending.Trending
 import com.google.firebase.auth.FirebaseAuth
@@ -57,7 +63,7 @@ class MainActivity : ComponentActivity() {
           Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
             val userIdState = remember { mutableStateOf<String?>(null) }
             if (userIdState.value == null) {
-                WelcomeScreen2(onSignInSuccess = { userId -> userIdState.value = userId })
+                WelcomeScreen(onSignInSuccess = { userId -> userIdState.value = userId })
             } else {
                 val nav = rememberNavController()
                 Log.d("SignIn", "Signed in state changed: ${userIdState.value}")
@@ -83,58 +89,4 @@ class MainActivity : ComponentActivity() {
       }
     }
   }
-    @Composable
-    fun WelcomeScreen2(onSignInSuccess: (String) -> Unit) {
-        val launcher =
-            rememberLauncherForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
-                if (res.resultCode == RESULT_OK) {
-                    val user = FirebaseAuth.getInstance().currentUser
-                    user?.uid?.let { userId -> onSignInSuccess(userId) }
-                } else {
-                    // Sign-in error
-                }
-            }
-
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.GoogleBuilder().build()
-        )
-        val signInIntent =
-            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
-
-        Column(
-            modifier = Modifier.padding(15.dp).width(258.dp).height(510.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                modifier = Modifier.width(258.dp).height(65.dp),
-                text = "Welcome",
-
-                // M3/display/large
-                style =
-                TextStyle(
-                    fontSize = 57.sp,
-                    lineHeight = 64.sp,
-                    fontFamily = FontFamily(Font(R.font.roboto)),
-                    fontWeight = FontWeight(400),
-                    color = Color(0xFF191C1E),
-                    textAlign = TextAlign.Center,
-                )
-            )
-
-            Spacer(modifier = Modifier.height(150.dp))
-
-            OutlinedButton(
-                onClick = { launcher.launch(signInIntent) },
-                shape = RoundedCornerShape(size = 20.dp),
-                modifier = Modifier.padding(start = 16.dp, end = 16.dp).wrapContentSize(),
-                colors = ButtonDefaults.outlinedButtonColors()) {
-                Image(
-                    modifier = Modifier.width(24.dp).height(24.dp),
-                    painter = painterResource(id = R.drawable.google_logo),
-                    contentDescription = "Google logo")
-
-                Text(modifier = Modifier.padding(6.dp), text = "Sign in with Google")
-            }
-        }
-    }
 }
--- app/src/main/java/com/github/se/gomeet/ui/WelcomeScreen.kt
+++ app/src/main/java/com/github/se/gomeet/ui/WelcomeScreen.kt
@@ -2,10 +2,15 @@ package com.github.se.gomeet.ui
 
 import android.app.Activity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,13 +26,21 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.android.animation.SegmentType
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.github.se.gomeet.R
@@ -38,7 +51,6 @@ import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun WelcomeScreen(onSignInSuccess: (String) -> Unit) {
-    /*
     val launcher =
         rememberLauncherForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
             if (res.resultCode == Activity.RESULT_OK) {
@@ -55,9 +67,6 @@ fun WelcomeScreen(onSignInSuccess: (String) -> Unit) {
     val signInIntent =
         AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
 
-     */
-
-
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
@@ -66,12 +75,15 @@ fun WelcomeScreen(onSignInSuccess: (String) -> Unit) {
             .background(Color.White)
             .padding(25.dp)
     ) {
+
+        Spacer(modifier = Modifier.size(70.dp))
+
         Image(
             painter = painterResource(id = R.drawable.gomeet_logo),
             contentDescription = "GoMeet Logo"
         )
 
-        Spacer(modifier = Modifier.size(40.dp))
+        Spacer(modifier = Modifier.size(30.dp))
 
         Text(
             text = "See what's happening around you right now.",
@@ -88,14 +100,16 @@ fun WelcomeScreen(onSignInSuccess: (String) -> Unit) {
         Spacer(modifier = Modifier.size(80.dp))
 
         OutlinedButton(
-            onClick = { /* TODO: launcher.launch(signInIntent) */ },
+            onClick = { launcher.launch(signInIntent) },
             modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp)
                 .width(250.dp)
-                .height(50.dp),
-            shape = RoundedCornerShape(size = 20.dp),
+                .height(40.dp),
+            shape = RoundedCornerShape(20.dp),
+            border = BorderStroke(1.dp, Color.Gray), // Set border here if needed
             enabled = true,
-            colors = ButtonDefaults.outlinedButtonColors(containerColor = Color.LightGray),
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = Color(0xFFECEFF1)
+            )
         ) {
             Image(
                 painter = painterResource(id = R.drawable.google_logo),
@@ -105,39 +119,71 @@ fun WelcomeScreen(onSignInSuccess: (String) -> Unit) {
                     .height(24.dp)
             )
 
-            Spacer(modifier = Modifier.size(6.dp))
+            Spacer(modifier = Modifier.size(15.dp)) // Adjust the size of the spacer as needed
 
             Text(
                 text = "Continue with Google",
-                modifier = Modifier.padding(6.dp),
-                color = Color.Black
+                color = Color.Black,
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp) // This will add padding around the text inside the button
             )
         }
 
-        Spacer(modifier = Modifier.size(70.dp))
+        Spacer(modifier = Modifier.size(1.dp))
+
+        DividerWithText()
+
+        Spacer(modifier = Modifier.size(1.dp))
 
         OutlinedButton(
             onClick = { /* TODO: email-password logic */ },
             modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp)
                 .width(250.dp)
-                .height(50.dp),
-            shape = RoundedCornerShape(size = 20.dp),
+                .height(40.dp),
+            shape = RoundedCornerShape(20.dp),
+            border = BorderStroke(1.dp, Color.Gray), // Set border here if needed
             enabled = true,
-            colors = ButtonDefaults.outlinedButtonColors(containerColor = Color.LightGray),
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = Color(0xFFECEFF1)
+            )
         ) {
             Text(
                 text = "Log in",
-                modifier = Modifier.padding(6.dp),
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp),
                 color = Color.Black
             )
         }
 
-        Text(
-            text = "Don't have an account? Create account",
-            style = MaterialTheme.typography.labelSmall,
-            modifier = Modifier.padding(top = 32.dp)
-        )
+
+
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Row(){
+            Text(
+                text = "Don’t have an account?",
+                style = TextStyle(
+                    fontSize = 11.sp,
+                    lineHeight = 17.sp,
+                    fontFamily = FontFamily(Font(R.font.roboto)),
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF3C4043),
+                    letterSpacing = 0.25.sp,
+                )
+            )
+
+            //TODO: Create account should go to RegisterScreen
+            Text(
+                text = "Create account",
+                style = TextStyle(
+                    fontSize = 11.sp,
+                    lineHeight = 17.sp,
+                    fontFamily = FontFamily(Font(R.font.roboto)),
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF2F6673),
+                    textAlign = TextAlign.Center,
+                    letterSpacing = 0.25.sp,
+                )
+            )
+        }
     }
 }
 
@@ -145,4 +191,45 @@ fun WelcomeScreen(onSignInSuccess: (String) -> Unit) {
 @Composable
 fun WelcomeScreenPreview() {
     WelcomeScreen {}
+}
+
+@Composable
+fun DividerWithText() {
+    val paint = Paint().apply {
+        color = Color.Black
+        strokeWidth = 5f
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Canvas(modifier = Modifier.matchParentSize()) {
+            val canvasWidth = size.width
+            val canvasHalfHeight = size.height / 2
+
+            drawLine(
+                color = Color.Black,
+                strokeWidth = 2f,
+                start = Offset(x = 120f, y = canvasHalfHeight),
+                end = Offset(x = (canvasWidth / 2) - 50f, y = canvasHalfHeight),
+            )
+            drawLine(
+                color = Color.Black,
+                strokeWidth = 2f,
+                start = Offset(x = (canvasWidth / 2) + 50f, y = canvasHalfHeight),
+                end = Offset(x = canvasWidth - 120, y = canvasHalfHeight),
+            )
+        }
+        Text(
+            text = "or",
+            modifier = Modifier.align(Alignment.Center),
+            style = TextStyle(
+                fontSize = 15.sp,
+                fontFamily = FontFamily(Font(R.font.roboto)),
+                fontWeight = FontWeight(700),
+                color = Color.Black,
+            )
+        )
+    }
 }
