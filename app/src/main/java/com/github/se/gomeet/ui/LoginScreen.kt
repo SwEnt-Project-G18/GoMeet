f45201125dcd45678e00f48cf2beb4503671969e
Antoine Garin <antoine.garin@epfl.ch>
2024-04-02 13:59:03 +0200

    restored secrets.defaults.properties

--- app/src/main/java/com/github/se/gomeet/ui/LoginScreen.kt
+++ app/src/main/java/com/github/se/gomeet/ui/LoginScreen.kt
@@ -3,9 +3,11 @@ package com.github.se.gomeet.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -29,28 +31,37 @@ fun LoginScreen(onLoginClicked: (String, String) -> Unit) {
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-            .padding(16.dp)
+            .padding(25.dp)
     ) {
         Text(
             text = "GoMeet",
             style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier.padding(bottom = 16.dp)
         )
+
+        Spacer(modifier = Modifier.size(30.dp))
+
         Text(
             text = "Login",
             style = MaterialTheme.typography.headlineSmall,
             modifier = Modifier.padding(bottom = 16.dp)
         )
+
+        Spacer(modifier = Modifier.size(170.dp))
+
         TextField(
             value = email,
             onValueChange = { newValue -> email = newValue },
             label = { Text("Email") },
             modifier = Modifier.fillMaxWidth()
         )
+
+        Spacer(modifier = Modifier.size(16.dp))
+
         TextField(
             value = password,
             onValueChange = { newValue -> password = newValue },
@@ -58,6 +69,9 @@ fun LoginScreen(onLoginClicked: (String, String) -> Unit) {
             visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier.fillMaxWidth()
         )
+
+        Spacer(modifier = Modifier.size(50.dp))
+
         Button(
             onClick = { onLoginClicked(email, password) },
             modifier = Modifier.fillMaxWidth()
