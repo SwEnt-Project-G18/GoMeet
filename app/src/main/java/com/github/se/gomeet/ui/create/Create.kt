8fc5729d2578712285cb5df6f2f08876034c327b
Antoine Garin <antoine.garin@epfl.ch>
2024-04-08 18:20:40 +0200

    started the UI screen to create the events

--- app/src/main/java/com/github/se/gomeet/ui/create/Create.kt
+++ app/src/main/java/com/github/se/gomeet/ui/create/Create.kt
@@ -1,20 +1,38 @@
 package com.github.se.gomeet.ui.create
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.github.se.gomeet.ui.navigation.BottomNavigationMenu
 import com.github.se.gomeet.ui.navigation.NavigationActions
 import com.github.se.gomeet.ui.navigation.Route
 import com.github.se.gomeet.ui.navigation.TOP_LEVEL_DESTINATIONS
+import com.github.se.gomeet.ui.theme.DarkerCyan
 
 @Composable
 fun Create(nav: NavigationActions) {
   Scaffold(
+      modifier = Modifier
+          .fillMaxSize()
+          .background(Color.White),
       bottomBar = {
         BottomNavigationMenu(
             onTabSelect = { selectedTab ->
@@ -23,7 +41,33 @@ fun Create(nav: NavigationActions) {
             tabList = TOP_LEVEL_DESTINATIONS,
             selectedItem = Route.EXPLORE)
       }) { innerPadding ->
-        Text("Create", modifier = Modifier.padding(innerPadding))
+
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start) {
+            Text(
+                text = "Create",
+                modifier = Modifier.padding(16.dp),
+                color = DarkerCyan,
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = FontFamily.Default,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineLarge)
+
+            Spacer(modifier = Modifier.size(120.dp))
+
+            Text(
+                text = "Choose your audience",
+                modifier = Modifier.padding(16.dp),
+                color = Color.Black,
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.Normal,
+                fontFamily = FontFamily.Default,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium)
+        }
       }
 }
 
