6b5c5bec6219bb4673e6741e286711f9a66a9784
vladdancau <vlad@dancau.ro>
2024-04-10 17:32:39 +0200

    Create ProfileTest.kt

---
+++ app/src/androidTest/java/com/github/se/gomeet/ui/profile/ProfileTest.kt
@@ -0,0 +1,35 @@
+package com.github.se.gomeet.ui.profile
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.rememberNavController
+import com.github.se.gomeet.ui.navigation.NavigationActions
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.internal.matchers.Null
+
+class ProfileTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun profileUiTest() {
+        composeTestRule.setContent {
+                Profile(NavigationActions(rememberNavController()))
+            }
+
+
+        composeTestRule.onNodeWithText("My Profile").assertIsDisplayed()
+
+        composeTestRule.onNodeWithContentDescription("image description").assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("Edit Profile").performClick()
+
+        composeTestRule.onNodeWithText("Share Profile").assertIsDisplayed()
+    }
+
+}
