4f51194b02dad9bc67daae6c92a370a12561cdd7
vladdancau <vlad@dancau.ro>
2024-04-10 11:21:17 +0200

    Profile screen top details bar

--- app/src/main/java/com/github/se/gomeet/ui/profile/Profile.kt
+++ app/src/main/java/com/github/se/gomeet/ui/profile/Profile.kt
@@ -1,26 +1,105 @@
 package com.github.se.gomeet.ui.profile
 
+import android.graphics.Paint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.github.se.gomeet.R
 import com.github.se.gomeet.ui.navigation.BottomNavigationMenu
 import com.github.se.gomeet.ui.navigation.NavigationActions
 import com.github.se.gomeet.ui.navigation.Route
 import com.github.se.gomeet.ui.navigation.TOP_LEVEL_DESTINATIONS
+import com.github.se.gomeet.ui.theme.DarkCyan
 
 @Composable
-fun Profile(nav: NavigationActions) {
-  Scaffold(
-      bottomBar = {
-        BottomNavigationMenu(
-            onTabSelect = { selectedTab ->
-              nav.navigateTo(TOP_LEVEL_DESTINATIONS.first { it.route == selectedTab })
-            },
-            tabList = TOP_LEVEL_DESTINATIONS,
-            selectedItem = Route.EXPLORE)
-      }) { innerPadding ->
-        Text("Profile", Modifier.padding(innerPadding))
-      }
+fun Profile(nav: NavigationActions) { // TODO Add parameters to the function
+    Scaffold(
+        bottomBar = {
+            BottomNavigationMenu(
+                onTabSelect = { selectedTab ->
+                    nav.navigateTo(TOP_LEVEL_DESTINATIONS.first { it.route == selectedTab })
+                },
+                tabList = TOP_LEVEL_DESTINATIONS,
+                selectedItem = Route.EXPLORE
+            )
+        }) { innerPadding ->
+        print(innerPadding)
+            Text(
+                text = "My Profile",
+                modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp),
+                color = DarkCyan,
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = FontFamily.Default,
+                textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.headlineLarge
+            )
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .padding(top = 100.dp)
+                .fillMaxSize()
+        )
+        {
+            Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+
+                Image(
+                    modifier = Modifier
+                        .padding(30.dp)
+                        .width(101.dp)
+                        .height(101.dp)
+                        .clip(CircleShape)
+                        .background(color = Color(0xFFD9D9D9)),
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "image description",
+                    contentScale = ContentScale.None
+                )
+                Column {
+                    Row {
+                        Text("Name", textAlign = TextAlign.Center)
+                        Spacer(modifier = Modifier.padding(2.dp))
+                        Text("Surname")
+                    }
+                    Text(text = "@username")
+                }
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProfilePreview() {
+  Profile(nav = NavigationActions(rememberNavController()))
 }
