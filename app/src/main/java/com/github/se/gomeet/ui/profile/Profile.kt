771f8a6739cf4807ac97e40080d149ce09f6d690
vladdancau <vlad@dancau.ro>
2024-04-10 17:08:48 +0200

    Complete profile page
    ViewModel missing, color schemes to be reviewed by team, size of the tags buttons and shape also to be reviewed.

--- app/src/main/java/com/github/se/gomeet/ui/profile/Profile.kt
+++ app/src/main/java/com/github/se/gomeet/ui/profile/Profile.kt
@@ -6,15 +6,29 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.IconButton
+import androidx.compose.material.darkColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Build
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -26,12 +40,16 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import com.github.se.gomeet.R
 import com.github.se.gomeet.ui.navigation.BottomNavigationMenu
@@ -39,6 +57,7 @@ import com.github.se.gomeet.ui.navigation.NavigationActions
 import com.github.se.gomeet.ui.navigation.Route
 import com.github.se.gomeet.ui.navigation.TOP_LEVEL_DESTINATIONS
 import com.github.se.gomeet.ui.theme.DarkCyan
+import com.github.se.gomeet.ui.theme.Grey
 
 @Composable
 fun Profile(nav: NavigationActions) { // TODO Add parameters to the function
@@ -53,16 +72,16 @@ fun Profile(nav: NavigationActions) { // TODO Add parameters to the function
             )
         }) { innerPadding ->
         print(innerPadding)
-            Text(
-                text = "My Profile",
-                modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp),
-                color = DarkCyan,
-                fontStyle = FontStyle.Normal,
-                fontWeight = FontWeight.SemiBold,
-                fontFamily = FontFamily.Default,
-                textAlign = TextAlign.Start,
-                style = MaterialTheme.typography.headlineLarge
-            )
+        Text(
+            text = "My Profile",
+            modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp),
+            color = DarkCyan,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.SemiBold,
+            fontFamily = FontFamily.Default,
+            textAlign = TextAlign.Start,
+            style = MaterialTheme.typography.headlineLarge
+        )
         Column(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start,
@@ -71,7 +90,11 @@ fun Profile(nav: NavigationActions) { // TODO Add parameters to the function
                 .fillMaxSize()
         )
         {
-            Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
 
                 Image(
                     modifier = Modifier
@@ -86,14 +109,308 @@ fun Profile(nav: NavigationActions) { // TODO Add parameters to the function
                 )
                 Column {
                     Row {
-                        Text("Name", textAlign = TextAlign.Center)
-                        Spacer(modifier = Modifier.padding(2.dp))
-                        Text("Surname")
+                        Text(
+                            "Name" + " " + "Surname", textAlign = TextAlign.Center,
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                lineHeight = 16.sp,
+                                fontFamily = FontFamily(Font(R.font.roboto)),
+                                fontWeight = FontWeight(500),
+                                color = Color(0xFF000000),
+
+                                textAlign = TextAlign.Center,
+                                letterSpacing = 0.5.sp,
+                            )
+
+                        )
                     }
-                    Text(text = "@username")
+                    Text(
+                        text = "@username",
+                        style = TextStyle(
+                            fontSize = 15.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFF000000),
+
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        )
+
+                    )
                 }
             }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .width(177.dp)
+                        .height(40.dp)
+                        .background(
+                            color = Color(0xFFECEFF1),
+                            shape = RoundedCornerShape(size = 10.dp)
+                        ),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFECEFF1)
+                    )
+                ) {
+                    Text(text = "Edit Profile", color = Color(0xFF000000))
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .width(177.dp)
+                        .height(40.dp)
+                        .background(
+                            color = Color(0xFFECEFF1),
+                            shape = RoundedCornerShape(size = 10.dp)
+                        ),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFECEFF1)
+                    )
+                ) {
+                    Text(text = "Share Profile", color = Color(0xFF000000))
+                }
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column {
+                    Text(
+                        text = "10",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFF2F6673),
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Text(
+                        text = "Events",
+                        style = TextStyle(
+                            fontSize = 13.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFF2F6673),
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                }
+                Divider(
+                    modifier = Modifier
+                        //.fillMaxHeight()
+                        .height(40.dp)
+                        .width(1.dp)
+                )
+                Column {
+                    Text(
+                        text = "10",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFF2F6673),
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Text(
+                        text = "Followers",
+                        style = TextStyle(
+                            fontSize = 13.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFF2F6673),
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
 
+                }
+                Divider(
+                    modifier = Modifier
+                        //.fillMaxHeight()
+                        .height(40.dp)
+                        .width(1.dp)
+                )
+                Column {
+                    Text(
+                        text = "10",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFF2F6673),
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Text(
+                        text = "Following",
+                        style = TextStyle(
+                            fontSize = 13.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFF2F6673),
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Column {
+                Text(
+                    text = "Tags",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        lineHeight = 16.sp,
+                        fontFamily = FontFamily(Font(R.font.roboto)),
+                        fontWeight = FontWeight(700),
+                        color = DarkCyan,
+                        textAlign = TextAlign.Center,
+                        letterSpacing = 0.5.sp,
+                    ),
+                    modifier = Modifier
+                        .width(74.dp)
+                        .height(17.dp)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                LazyRow(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(start = 12.dp, end = 8.dp),
+                    modifier = Modifier.heightIn(min = 56.dp)
+
+                ) {
+                    items(10) {
+                        Button(
+                            onClick = {},
+                            content = {
+                                Text("Test")
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = DarkCyan
+                            )
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Column {
+                Row {
+                    Text(
+                        text = "My Events",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(700),
+                            color = DarkCyan,
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                        modifier = Modifier
+                            .width(104.dp)
+                            .height(17.dp)
+                    )
+                    Text(text = "View all", color = Grey)
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                LazyRow(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(start = 12.dp, end = 8.dp),
+                    modifier = Modifier.heightIn(min = 56.dp)
+
+                ) {
+                    items(3) {
+                        Column{
+                            Image(
+                                painter = painterResource(id = R.drawable.chess_demo),
+                                contentDescription = "Event 1",
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .width(150.dp)
+                                    .height(60.dp)
+                                    .clip(RoundedCornerShape(size = 10.dp))
+                            )
+                            Text(text = "Chess Tournament", color = DarkCyan)
+                            Text(text = "11.04.2024 - 21:00", color = Grey)
+                        }
+
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Column {
+                Row {
+                    Text(
+                        text = "History",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(700),
+                            color = DarkCyan,
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                        modifier = Modifier
+                            .width(74.dp)
+                            .height(17.dp)
+                    )
+                    Text(text = "View all", color = Grey)
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                LazyRow(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(start = 12.dp, end = 8.dp),
+                    modifier = Modifier.heightIn(min = 56.dp)
+
+                ) {
+                    items(10) {
+                        Column{
+                            Image(
+                                painter = painterResource(id = R.drawable.chess_demo),
+                                contentDescription = "Event 1",
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .width(150.dp)
+                                    .height(60.dp)
+                                    .clip(RoundedCornerShape(size = 10.dp))
+                            )
+                            Text(text = "Chess Tournament", color = DarkCyan)
+                            Text(text = "11.04.2024 - 21:00", color = Grey)
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -101,5 +418,5 @@ fun Profile(nav: NavigationActions) { // TODO Add parameters to the function
 @Preview
 @Composable
 fun ProfilePreview() {
-  Profile(nav = NavigationActions(rememberNavController()))
+    Profile(nav = NavigationActions(rememberNavController()))
 }
