6773e37964dad33a0af59c2a379b1d1f66d05c16
vladdancau <vlad@dancau.ro>
2024-04-12 11:18:59 +0200

    Events start

--- app/src/main/java/com/github/se/gomeet/ui/mainscreens/Events.kt
+++ app/src/main/java/com/github/se/gomeet/ui/mainscreens/Events.kt
@@ -3,8 +3,10 @@ package com.github.se.gomeet.ui.mainscreens
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.github.se.gomeet.ui.navigation.BottomNavigationMenu
 import com.github.se.gomeet.ui.navigation.NavigationActions
 import com.github.se.gomeet.ui.navigation.Route
@@ -14,6 +16,9 @@ import com.github.se.gomeet.viewmodel.EventViewModel
 @Composable
 fun Events(nav: NavigationActions, eventViewModel: EventViewModel) {
   Scaffold(
+      topBar = {
+        Text(text = "Test")
+      },
       bottomBar = {
         BottomNavigationMenu(
             onTabSelect = { selectedTab ->
@@ -29,5 +34,5 @@ fun Events(nav: NavigationActions, eventViewModel: EventViewModel) {
 
 @Composable
 fun EventWidget() {
-  Text("Event Widget")
+    Events(nav = NavigationActions(rememberNavController()), eventViewModel = EventViewModel())
 }
