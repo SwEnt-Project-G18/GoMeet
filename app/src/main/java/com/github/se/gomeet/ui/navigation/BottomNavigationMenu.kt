801dbe2b2df494d5b53e7bce64e842b8cb3291b5
albertfares <albert_fares@hotmail.com>
2024-04-12 10:59:15 +0200

    ktfmt

--- app/src/main/java/com/github/se/gomeet/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/github/se/gomeet/ui/navigation/BottomNavigationMenu.kt
@@ -1,6 +1,5 @@
 package com.github.se.gomeet.ui.navigation
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.size
@@ -15,8 +14,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.se.gomeet.ui.theme.Cyan
-import com.github.se.gomeet.ui.theme.DarkModeBackground
-import com.github.se.gomeet.ui.theme.SomewhatWhite
 import com.github.se.gomeet.ui.theme.TranslucentCyan
 
 @Composable
@@ -25,32 +22,29 @@ fun BottomNavigationMenu(
     tabList: List<TopLevelDestination>,
     selectedItem: String
 ) {
-    NavigationBar(
-        modifier = Modifier.navigationBarsPadding().height(80.dp),
-        containerColor = Color.Transparent,
-        tonalElevation = 0.dp,
-    )
-    {
-        tabList.forEach { destination ->
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = getIconForRoute(destination.route),
-                        contentDescription = destination.textId,
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                label = { Text(destination.textId) },
-                selected = destination.route == selectedItem,
-                onClick = { onTabSelect(destination.route) },
-                colors = NavigationBarItemDefaults.colors(
-                    selectedTextColor = Cyan,
-                    selectedIconColor = Cyan,
-                    indicatorColor = TranslucentCyan
-                )
-            )
-        }
+  NavigationBar(
+      modifier = Modifier.navigationBarsPadding().height(80.dp),
+      containerColor = Color.Transparent,
+      tonalElevation = 0.dp,
+  ) {
+    tabList.forEach { destination ->
+      NavigationBarItem(
+          icon = {
+            Icon(
+                imageVector = getIconForRoute(destination.route),
+                contentDescription = destination.textId,
+                modifier = Modifier.size(24.dp))
+          },
+          label = { Text(destination.textId) },
+          selected = destination.route == selectedItem,
+          onClick = { onTabSelect(destination.route) },
+          colors =
+              NavigationBarItemDefaults.colors(
+                  selectedTextColor = Cyan,
+                  selectedIconColor = Cyan,
+                  indicatorColor = TranslucentCyan))
     }
+  }
 }
 
 @Preview
