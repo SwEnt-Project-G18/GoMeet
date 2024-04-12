557df9b765af2e693c11199086aa93500c37f000
vladdancau <vlad@dancau.ro>
2024-04-12 11:59:20 +0200

    Update Events.kt

--- app/src/main/java/com/github/se/gomeet/ui/mainscreens/Events.kt
+++ app/src/main/java/com/github/se/gomeet/ui/mainscreens/Events.kt
@@ -1,23 +1,46 @@
 package com.github.se.gomeet.ui.mainscreens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
+import com.github.se.gomeet.R
 import com.github.se.gomeet.ui.navigation.BottomNavigationMenu
 import com.github.se.gomeet.ui.navigation.NavigationActions
 import com.github.se.gomeet.ui.navigation.Route
 import com.github.se.gomeet.ui.navigation.TOP_LEVEL_DESTINATIONS
-import com.github.se.gomeet.viewmodel.EventViewModel
+import com.github.se.gomeet.ui.theme.DarkCyan
 
 @Composable
-fun Events(nav: NavigationActions, eventViewModel: EventViewModel) {
+fun Events(nav: NavigationActions) {
   Scaffold(
       topBar = {
-        Text(text = "Test")
+        Text(
+            text = "Events",
+            style =
+                TextStyle(
+                    fontSize = 24.sp,
+                    lineHeight = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.roboto)),
+                    fontWeight = FontWeight(700),
+                    textAlign = TextAlign.Center,
+                    color = DarkCyan,
+                    letterSpacing = 0.5.sp,
+                ),
+            modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp))
       },
       bottomBar = {
         BottomNavigationMenu(
@@ -27,12 +50,22 @@ fun Events(nav: NavigationActions, eventViewModel: EventViewModel) {
             tabList = TOP_LEVEL_DESTINATIONS,
             selectedItem = Route.EXPLORE)
       }) { innerPadding ->
-        Text("Events", Modifier.padding(innerPadding))
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.padding(innerPadding)) {
 
+        }
       }
 }
 
 @Composable
 fun EventWidget() {
-    Events(nav = NavigationActions(rememberNavController()), eventViewModel = EventViewModel())
+  Text("Event Widget")
+}
+
+@Composable
+@Preview
+fun EventPreview() {
+  Events(nav = NavigationActions(rememberNavController()))
 }
