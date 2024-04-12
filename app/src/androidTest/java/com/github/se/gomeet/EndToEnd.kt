c00bfadbc8e1af3c03ef6f32b3b64e275d3dc069
chorfi <chorfiyouss@gmail.com>
2024-04-12 21:31:10 +0200

    conflicts

--- app/src/androidTest/java/com/github/se/gomeet/EndToEnd.kt
+++ app/src/androidTest/java/com/github/se/gomeet/EndToEnd.kt
@@ -14,7 +14,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class EndToEndTest2 : TestCase() {
+class EndToEndTest : TestCase() {
 
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
