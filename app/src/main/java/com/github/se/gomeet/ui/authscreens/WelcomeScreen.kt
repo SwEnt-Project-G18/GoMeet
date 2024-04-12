7485a17ab88aeb78e2d01e3862165db83a1d0029
albertfares <albert_fares@hotmail.com>
2024-04-12 16:35:13 +0200

    ktfmt

--- app/src/main/java/com/github/se/gomeet/ui/authscreens/WelcomeScreen.kt
+++ app/src/main/java/com/github/se/gomeet/ui/authscreens/WelcomeScreen.kt
@@ -5,7 +5,6 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
