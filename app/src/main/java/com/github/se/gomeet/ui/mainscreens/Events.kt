8961051563091b8b3663a6686867621e63fb19e0
vladdancau <vlad@dancau.ro>
2024-04-12 18:57:12 +0200

    UI done for Events list
    Missing search bar; will be integrated later into the app

--- app/src/main/java/com/github/se/gomeet/ui/mainscreens/Events.kt
+++ app/src/main/java/com/github/se/gomeet/ui/mainscreens/Events.kt
@@ -1,13 +1,32 @@
 package com.github.se.gomeet.ui.mainscreens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -23,6 +42,7 @@ import com.github.se.gomeet.ui.navigation.NavigationActions
 import com.github.se.gomeet.ui.navigation.Route
 import com.github.se.gomeet.ui.navigation.TOP_LEVEL_DESTINATIONS
 import com.github.se.gomeet.ui.theme.DarkCyan
+import com.github.se.gomeet.ui.theme.NavBarUnselected
 
 @Composable
 fun Events(nav: NavigationActions) {
@@ -35,7 +55,7 @@ fun Events(nav: NavigationActions) {
                     fontSize = 24.sp,
                     lineHeight = 16.sp,
                     fontFamily = FontFamily(Font(R.font.roboto)),
-                    fontWeight = FontWeight(700),
+                    fontWeight = FontWeight.SemiBold,
                     textAlign = TextAlign.Center,
                     color = DarkCyan,
                     letterSpacing = 0.5.sp,
@@ -52,20 +72,204 @@ fun Events(nav: NavigationActions) {
       }) { innerPadding ->
         Column(
             verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(innerPadding)) {
+              Spacer(modifier = Modifier.height(10.dp))
+              Row(
+                  verticalAlignment = Alignment.CenterVertically,
+                  horizontalArrangement = Arrangement.Center,
+                  modifier = Modifier.heightIn(min = 56.dp).fillMaxWidth()) {
+                    Button(
+                        onClick = {},
+                        content = { Text("My tickets") },
+                        colors =
+                            ButtonDefaults.buttonColors(
+                                containerColor = NavBarUnselected, contentColor = DarkCyan),
+                        border = BorderStroke(1.dp, DarkCyan),
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Button(
+                        onClick = {},
+                        content = { Text("Favourites") },
+                        colors =
+                            ButtonDefaults.buttonColors(
+                                containerColor = NavBarUnselected, contentColor = DarkCyan),
+                        border = BorderStroke(1.dp, DarkCyan),
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Button(
+                        onClick = {},
+                        content = { Text("My events") },
+                        colors =
+                            ButtonDefaults.buttonColors(
+                                containerColor = NavBarUnselected, contentColor = DarkCyan),
+                        border = BorderStroke(1.dp, DarkCyan),
+                    )
+                  }
+              Text(
+                  text = "My Tickets",
+                  style =
+                      TextStyle(
+                          fontSize = 16.sp,
+                          lineHeight = 16.sp,
+                          fontFamily = FontFamily(Font(R.font.roboto)),
+                          fontWeight = FontWeight(700),
+                          color = DarkCyan,
+                          textAlign = TextAlign.Center,
+                          letterSpacing = 0.5.sp,
+                      ),
+                  modifier = Modifier.padding(10.dp).align(Alignment.Start))
+              EventWidget(
+                  UserName = "EPFL Chess Club",
+                  EventName = "Chess Tournament",
+                  EventDate = "WEDNESDAY - 14:00",
+                  ProfilePicture = R.drawable.gomeet_logo,
+                  EventPicture = R.drawable.intbee_logo,
+                  Verified = true)
+              Spacer(modifier = Modifier.height(10.dp))
+              EventWidget(
+                  UserName = "EPFL Chess Club",
+                  EventName = "Chess Tournament",
+                  EventDate = "WEDNESDAY - 14:00",
+                  ProfilePicture = R.drawable.gomeet_logo,
+                  EventPicture = R.drawable.intbee_logo,
+                  Verified = true)
+            Spacer(modifier = Modifier.height(10.dp))
+            EventWidget(
+                UserName = "EPFL Chess Club",
+                EventName = "Chess Tournament",
+                EventDate = "WEDNESDAY - 14:00",
+                ProfilePicture = R.drawable.gomeet_logo,
+                EventPicture = R.drawable.intbee_logo,
+                Verified = true)
 
-        }
+              Text(
+                  text = "Favourites",
+                  style =
+                      TextStyle(
+                          fontSize = 16.sp,
+                          lineHeight = 16.sp,
+                          fontFamily = FontFamily(Font(R.font.roboto)),
+                          fontWeight = FontWeight(700),
+                          color = DarkCyan,
+                          textAlign = TextAlign.Center,
+                          letterSpacing = 0.5.sp,
+                      ),
+                  modifier = Modifier.padding(10.dp).align(Alignment.Start))
+            EventWidget(
+                UserName = "EPFL Chess Club",
+                EventName = "Chess Tournament",
+                EventDate = "WEDNESDAY - 14:00",
+                ProfilePicture = R.drawable.gomeet_logo,
+                EventPicture = R.drawable.intbee_logo,
+                Verified = true)
+              Text(
+                  text = "My Events",
+                  style =
+                      TextStyle(
+                          fontSize = 16.sp,
+                          lineHeight = 16.sp,
+                          fontFamily = FontFamily(Font(R.font.roboto)),
+                          fontWeight = FontWeight(700),
+                          color = DarkCyan,
+                          textAlign = TextAlign.Center,
+                          letterSpacing = 0.5.sp,
+                      ),
+                  modifier = Modifier.padding(10.dp).align(Alignment.Start))
+            EventWidget(
+                UserName = "EPFL Chess Club",
+                EventName = "Chess Tournament",
+                EventDate = "WEDNESDAY - 14:00",
+                ProfilePicture = R.drawable.gomeet_logo,
+                EventPicture = R.drawable.intbee_logo,
+                Verified = true)
+            }
       }
 }
 
 @Composable
-fun EventWidget() {
-  Text("Event Widget")
+fun EventWidget(
+    UserName: String,
+    EventName: String,
+    EventDate: String,
+    ProfilePicture: Int,
+    EventPicture: Int,
+    Verified: Boolean
+) {
+  Card(
+      modifier = Modifier.width(377.dp).height(77.dp),
+      colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+      border = BorderStroke(1.dp, DarkCyan)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center) {
+              Image(
+                  painterResource(id = ProfilePicture),
+                  "Event Picture",
+                  modifier = Modifier.padding(15.dp).width(40.dp).height(40.dp))
+              Column {
+                Row {
+                  Text(
+                      UserName,
+                      style =
+                          TextStyle(
+                              fontSize = 16.sp,
+                              lineHeight = 24.sp,
+                              fontFamily = FontFamily(Font(R.font.roboto)),
+                              fontWeight = FontWeight(700),
+                              color = Color(0xFF1D1B20),
+                              letterSpacing = 0.15.sp,
+                          ),
+                      modifier = Modifier.padding(top = 5.dp))
+                  if (Verified) {
+                    Icon(
+                        imageVector = Icons.Default.CheckCircle,
+                        contentDescription = "Verified",
+                        modifier = Modifier.padding(5.dp))
+                  }
+                }
+                Text(
+                    text = EventName,
+                    style =
+                        TextStyle(
+                            fontSize = 14.sp,
+                            lineHeight = 20.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(700),
+                            color = Color(0xFF1D1B20),
+                            letterSpacing = 0.25.sp,
+                        ))
+                Text(
+                    EventDate,
+                    style =
+                        TextStyle(
+                            fontSize = 14.sp,
+                            lineHeight = 20.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(700),
+                            color = Color(0xFF1D1B20),
+                            letterSpacing = 0.25.sp,
+                        ))
+              }
+              Image(
+                  painterResource(id = EventPicture),
+                  "Event Picture",
+                  modifier = Modifier.width(80.dp).height(77.dp).padding(10.dp),
+                  contentScale = ContentScale.FillBounds)
+            }
+      }
 }
 
 @Composable
 @Preview
 fun EventPreview() {
   Events(nav = NavigationActions(rememberNavController()))
+  /*EventWidget(
+  "EPFL Chess Club",
+  "Chess Tournament",
+  "WEDNESDAY - 14:00",
+  R.drawable.gomeet_logo,
+  R.drawable.intbee_logo,
+  true)*/
 }
