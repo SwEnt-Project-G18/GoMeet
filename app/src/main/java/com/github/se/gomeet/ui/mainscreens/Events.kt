0bae35cd08e6fa827d0603fca243745ed2dfa359
albertfares <albert_fares@hotmail.com>
2024-04-16 16:04:54 +0200

    modified the scalings of an event

--- app/src/main/java/com/github/se/gomeet/ui/mainscreens/Events.kt
+++ app/src/main/java/com/github/se/gomeet/ui/mainscreens/Events.kt
@@ -3,14 +3,21 @@ package com.github.se.gomeet.ui.mainscreens
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
@@ -24,12 +31,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,24 +56,35 @@ import com.github.se.gomeet.ui.navigation.Route
 import com.github.se.gomeet.ui.navigation.TOP_LEVEL_DESTINATIONS
 import com.github.se.gomeet.ui.theme.DarkCyan
 import com.github.se.gomeet.ui.theme.NavBarUnselected
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun Events(nav: NavigationActions) {
+
+
+    val eventDate = Calendar.getInstance().apply {
+        set(Calendar.YEAR, 2024)
+        set(Calendar.MONTH, Calendar.APRIL)
+        set(Calendar.DAY_OF_MONTH, 16)
+        set(Calendar.HOUR_OF_DAY, 14)
+        set(Calendar.MINUTE, 0)
+    }.time
+
+
   Scaffold(
       topBar = {
         Text(
             text = "Events",
-            style =
-                TextStyle(
-                    fontSize = 24.sp,
-                    lineHeight = 16.sp,
-                    fontFamily = FontFamily(Font(R.font.roboto)),
-                    fontWeight = FontWeight.SemiBold,
-                    textAlign = TextAlign.Center,
-                    color = DarkCyan,
-                    letterSpacing = 0.5.sp,
-                ),
-            modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp))
+            modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp),
+            color = DarkCyan,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.SemiBold,
+            fontFamily = FontFamily.Default,
+            textAlign = TextAlign.Start,
+            style = MaterialTheme.typography.headlineLarge)
       },
       bottomBar = {
         BottomNavigationMenu(
@@ -106,83 +130,105 @@ fun Events(nav: NavigationActions) {
                         border = BorderStroke(1.dp, DarkCyan),
                     )
                   }
-              Text(
-                  text = "My Tickets",
-                  style =
-                      TextStyle(
-                          fontSize = 16.sp,
-                          lineHeight = 16.sp,
-                          fontFamily = FontFamily(Font(R.font.roboto)),
-                          fontWeight = FontWeight(700),
-                          color = DarkCyan,
-                          textAlign = TextAlign.Center,
-                          letterSpacing = 0.5.sp,
-                      ),
-                  modifier = Modifier.padding(10.dp).align(Alignment.Start))
-              EventWidget(
-                  UserName = "EPFL Chess Club",
-                  EventName = "Chess Tournament",
-                  EventDate = "WEDNESDAY - 14:00",
-                  ProfilePicture = R.drawable.gomeet_logo,
-                  EventPicture = R.drawable.intbee_logo,
-                  Verified = true)
-              Spacer(modifier = Modifier.height(10.dp))
-              EventWidget(
-                  UserName = "EPFL Chess Club",
-                  EventName = "Chess Tournament",
-                  EventDate = "WEDNESDAY - 14:00",
-                  ProfilePicture = R.drawable.gomeet_logo,
-                  EventPicture = R.drawable.intbee_logo,
-                  Verified = true)
-              Spacer(modifier = Modifier.height(10.dp))
-              EventWidget(
-                  UserName = "EPFL Chess Club",
-                  EventName = "Chess Tournament",
-                  EventDate = "WEDNESDAY - 14:00",
-                  ProfilePicture = R.drawable.gomeet_logo,
-                  EventPicture = R.drawable.intbee_logo,
-                  Verified = true)
 
-              Text(
-                  text = "Favourites",
-                  style =
-                      TextStyle(
-                          fontSize = 16.sp,
-                          lineHeight = 16.sp,
-                          fontFamily = FontFamily(Font(R.font.roboto)),
-                          fontWeight = FontWeight(700),
-                          color = DarkCyan,
-                          textAlign = TextAlign.Center,
-                          letterSpacing = 0.5.sp,
-                      ),
-                  modifier = Modifier.padding(10.dp).align(Alignment.Start))
-              EventWidget(
-                  UserName = "EPFL Chess Club",
-                  EventName = "Chess Tournament",
-                  EventDate = "WEDNESDAY - 14:00",
-                  ProfilePicture = R.drawable.gomeet_logo,
-                  EventPicture = R.drawable.intbee_logo,
-                  Verified = true)
-              Text(
-                  text = "My Events",
-                  style =
-                      TextStyle(
-                          fontSize = 16.sp,
-                          lineHeight = 16.sp,
-                          fontFamily = FontFamily(Font(R.font.roboto)),
-                          fontWeight = FontWeight(700),
-                          color = DarkCyan,
-                          textAlign = TextAlign.Center,
-                          letterSpacing = 0.5.sp,
-                      ),
-                  modifier = Modifier.padding(10.dp).align(Alignment.Start))
-              EventWidget(
-                  UserName = "EPFL Chess Club",
-                  EventName = "Chess Tournament",
-                  EventDate = "WEDNESDAY - 14:00",
-                  ProfilePicture = R.drawable.gomeet_logo,
-                  EventPicture = R.drawable.intbee_logo,
-                  Verified = true)
+              Column(modifier = Modifier.verticalScroll(rememberScrollState()).fillMaxSize()) {
+                Text(
+                    text = "My Tickets",
+                    style =
+                        TextStyle(
+                            fontSize = 20.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(1000),
+                            color = DarkCyan,
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                    modifier = Modifier.padding(10.dp).align(Alignment.Start))
+                EventWidget(
+                    UserName = "EPFL Chess Club",
+                    EventName = "Chess Tournament",
+                    EventDate = eventDate,
+                    ProfilePicture = R.drawable.gomeet_logo,
+                    EventPicture = R.drawable.intbee_logo,
+                    Verified = true)
+                EventWidget(
+                    UserName = "EPFL Chess Club",
+                    EventName = "Chess Tournament",
+                    EventDate = eventDate,
+                    ProfilePicture = R.drawable.gomeet_logo,
+                    EventPicture = R.drawable.intbee_logo,
+                    Verified = true)
+                EventWidget(
+                    UserName = "EPFL Chess Club",
+                    EventName = "Chess Tournament",
+                    EventDate = eventDate,
+                    ProfilePicture = R.drawable.gomeet_logo,
+                    EventPicture = R.drawable.intbee_logo,
+                    Verified = true)
+                  EventWidget(
+                      UserName = "EPFL Chess Club",
+                      EventName = "Chess Tournament",
+                      EventDate = eventDate,
+                      ProfilePicture = R.drawable.gomeet_logo,
+                      EventPicture = R.drawable.intbee_logo,
+                      Verified = true)
+                  EventWidget(
+                      UserName = "EPFL Chess Club",
+                      EventName = "Chess Tournament",
+                      EventDate = eventDate,
+                      ProfilePicture = R.drawable.gomeet_logo,
+                      EventPicture = R.drawable.intbee_logo,
+                      Verified = true)
+                  EventWidget(
+                      UserName = "EPFL Chess Club",
+                      EventName = "Chess Tournament",
+                      EventDate = eventDate,
+                      ProfilePicture = R.drawable.gomeet_logo,
+                      EventPicture = R.drawable.intbee_logo,
+                      Verified = true)
+
+                Text(
+                    text = "Favourites",
+                    style =
+                        TextStyle(
+                            fontSize = 20.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(1000),
+                            color = DarkCyan,
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                    modifier = Modifier.padding(10.dp).align(Alignment.Start))
+                EventWidget(
+                    UserName = "EPFL Chess Club",
+                    EventName = "Chess Tournament",
+                    EventDate = eventDate,
+                    ProfilePicture = R.drawable.gomeet_logo,
+                    EventPicture = R.drawable.intbee_logo,
+                    Verified = true)
+                Text(
+                    text = "My Events",
+                    style =
+                        TextStyle(
+                            fontSize = 20.sp,
+                            lineHeight = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(1000),
+                            color = DarkCyan,
+                            textAlign = TextAlign.Center,
+                            letterSpacing = 0.5.sp,
+                        ),
+                    modifier = Modifier.padding(10.dp).align(Alignment.Start))
+                EventWidget(
+                    UserName = "EPFL Chess Club",
+                    EventName = "Chess Tournament",
+                    EventDate = eventDate,
+                    ProfilePicture = R.drawable.gomeet_logo,
+                    EventPicture = R.drawable.intbee_logo,
+                    Verified = true)
+              }
             }
       }
 }
@@ -191,30 +237,62 @@ fun Events(nav: NavigationActions) {
 fun EventWidget(
     UserName: String,
     EventName: String,
-    EventDate: String,
+    EventDate: Date,
     ProfilePicture: Int,
     EventPicture: Int,
     Verified: Boolean
 ) {
+
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val density = LocalDensity.current
+
+    // Example logic to calculate text size based on screen width
+    val smallTextSize = with(density) { screenWidth.toPx() / 85 }
+    val bigTextSize = with(density) { screenWidth.toPx() / 60 }
+
+
   Card(
-      modifier = Modifier.width(377.dp).height(77.dp),
+      modifier =
+          Modifier.fillMaxWidth().padding(start = 10.dp, top = 5.dp, end = 10.dp, bottom = 5.dp),
       colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
-      border = BorderStroke(1.dp, DarkCyan)) {
+      border = BorderStroke(2.dp, DarkCyan)) {
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center) {
-              Image(
-                  painterResource(id = ProfilePicture),
-                  "Event Picture",
-                  modifier = Modifier.padding(15.dp).width(40.dp).height(40.dp))
-              Column {
-                Row {
+            horizontalArrangement = Arrangement.SpaceAround) {
+
+//              Image(
+//                  painterResource(id = ProfilePicture),
+//                  "Event Picture",
+//                  modifier = Modifier.weight(0.6f).padding(10.dp).aspectRatio(1f),
+//                  contentScale = ContentScale.Fit)
+
+              Column (   modifier = Modifier.weight(4f).padding(15.dp),
+                  horizontalAlignment = Alignment.Start, // Align text horizontally to center
+                  verticalArrangement = Arrangement.Center){
+
+
+                  Text(
+                      text = EventName,
+                      style =
+                      TextStyle(
+                          fontSize = bigTextSize.sp,
+                          lineHeight = 20.sp,
+                          fontFamily = FontFamily(Font(R.font.roboto)),
+                          fontWeight = FontWeight(700),
+                          color = Color(0xFF1D1B20),
+                          letterSpacing = 0.25.sp,
+                      ))
+
+
+                Row ( verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center){
+
                   Text(
                       UserName,
                       style =
                           TextStyle(
-                              fontSize = 16.sp,
+                              fontSize = smallTextSize.sp,
                               lineHeight = 24.sp,
                               fontFamily = FontFamily(Font(R.font.roboto)),
                               fontWeight = FontWeight(700),
@@ -223,28 +301,45 @@ fun EventWidget(
                           ),
                       modifier = Modifier.padding(top = 5.dp))
                   if (Verified) {
-                    Icon(
-                        imageVector = Icons.Default.CheckCircle,
-                        contentDescription = "Verified",
-                        modifier = Modifier.padding(5.dp))
+                      Box(
+                          contentAlignment = Alignment.Center,
+                          modifier = Modifier.padding(5.dp).size(smallTextSize.dp*(1.4f))
+                      ) {
+                          Image(
+                              painter = painterResource(id = R.drawable.verified),
+                              contentDescription = "Verified",
+                          )
+                      }
+
                   }
                 }
-                Text(
-                    text = EventName,
+
+                  val currentDate = Calendar.getInstance()
+                  val startOfWeek = currentDate.clone() as Calendar
+                  startOfWeek[Calendar.DAY_OF_WEEK] = startOfWeek.firstDayOfWeek
+                  val endOfWeek = startOfWeek.clone() as Calendar
+                  endOfWeek.add(Calendar.DAY_OF_WEEK, 6)
+
+                  val eventCalendar = Calendar.getInstance().apply { time = EventDate }
+
+                  // Determine if the event date is within this week
+                  val isThisWeek = eventCalendar >= startOfWeek && eventCalendar <= endOfWeek
+
+                  // Format based on whether the date is in the current week
+                  val dateFormat = if (isThisWeek) {
+                      SimpleDateFormat("EEEE - HH:mm", Locale.getDefault()) // "Tuesday"
+                  } else {
+                      SimpleDateFormat("dd/MM/yy - HH:mm", Locale.getDefault()) // "05/12/24"
+                  }
+
+                  // Convert the Date object to a formatted String
+                  val dateString = dateFormat.format(EventDate)
+
+                  Text(
+                    dateString,
                     style =
                         TextStyle(
-                            fontSize = 14.sp,
-                            lineHeight = 20.sp,
-                            fontFamily = FontFamily(Font(R.font.roboto)),
-                            fontWeight = FontWeight(700),
-                            color = Color(0xFF1D1B20),
-                            letterSpacing = 0.25.sp,
-                        ))
-                Text(
-                    EventDate,
-                    style =
-                        TextStyle(
-                            fontSize = 14.sp,
+                            fontSize = smallTextSize.sp,
                             lineHeight = 20.sp,
                             fontFamily = FontFamily(Font(R.font.roboto)),
                             fontWeight = FontWeight(700),
@@ -252,12 +347,17 @@ fun EventWidget(
                             letterSpacing = 0.25.sp,
                         ))
               }
-              Image(
-                  painterResource(id = EventPicture),
-                  "Event Picture",
-                  modifier = Modifier.width(80.dp).height(77.dp).padding(10.dp),
-                  contentScale = ContentScale.FillBounds)
-            }
+            Image(
+                painter = painterResource(id = EventPicture),
+                contentDescription = "Event Picture",
+                modifier = Modifier
+                    .weight(3f) // Take 1/3 of the card space because of the total weight of 4 (3 for the column and 1 for this image)
+                    .fillMaxHeight() // Fill the height of the Row
+                    .aspectRatio(3f / 1.75f) // Maintain an aspect ratio of 3:2, change it as needed
+                    .clipToBounds().padding(0.dp), // Clip the image if it overflows its bounds
+                contentScale = ContentScale.Crop // Crop the image to fit the aspect ratio
+            )
+        }
       }
 }
 
@@ -268,7 +368,7 @@ fun EventPreview() {
   /*EventWidget(
   "EPFL Chess Club",
   "Chess Tournament",
-  "WEDNESDAY - 14:00",
+  eventDate,
   R.drawable.gomeet_logo,
   R.drawable.intbee_logo,
   true)*/
