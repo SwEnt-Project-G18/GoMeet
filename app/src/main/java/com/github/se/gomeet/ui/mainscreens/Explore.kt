774f664059a4209d5f34e0832736d10fcd11b87e
koma2323 <kyykyy11111@gmail.com>
2024-04-22 11:33:14 +0200

    added a button to move camera to current location

--- app/src/main/java/com/github/se/gomeet/ui/mainscreens/Explore.kt
+++ app/src/main/java/com/github/se/gomeet/ui/mainscreens/Explore.kt
@@ -1,14 +1,16 @@
 package com.github.se.gomeet.ui.mainscreens
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,17 +24,23 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import com.github.se.gomeet.R
 import com.github.se.gomeet.model.event.Event
 import com.github.se.gomeet.ui.navigation.BottomNavigationMenu
 import com.github.se.gomeet.ui.navigation.NavigationActions
 import com.github.se.gomeet.ui.navigation.Route
 import com.github.se.gomeet.ui.navigation.TOP_LEVEL_DESTINATIONS
+import com.github.se.gomeet.ui.theme.DarkCyan
 import com.github.se.gomeet.viewmodel.EventViewModel
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority.PRIORITY_BALANCED_POWER_ACCURACY
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -45,19 +53,18 @@ import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.MarkerInfoWindowContent
 import com.google.maps.android.compose.rememberMarkerState
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
-private val defaultPosition = LatLng(46.51821074855688, 6.627076937989363)
+private val defaultPosition = LatLng(46.51912357457158, 6.568023741881372)
 private val deafaultZoom = 16f
+
+private var moveToCurrentLocation = mutableStateOf(false)
 
 @Composable
 fun Explore(nav: NavigationActions, eventViewModel: EventViewModel) {
   val coroutineScope = rememberCoroutineScope()
-  var eventList = remember { mutableListOf<Event>() }
-  val query = remember { mutableStateOf("") }
   val context = LocalContext.current
 
   val locationPermitted: MutableState<Boolean?> = remember { mutableStateOf(null) }
@@ -74,8 +81,10 @@ fun Explore(nav: NavigationActions, eventViewModel: EventViewModel) {
                 permissions.values.reduce { acc, isPermissionGranted -> acc && isPermissionGranted }
           })
   val locationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
-  var currentPosition by remember { mutableStateOf(defaultPosition) }
 
+  var eventList = remember { mutableListOf<Event>() }
+  val query = remember { mutableStateOf("") }
+  var currentPosition by remember { mutableStateOf(defaultPosition) }
   var isMapLoaded by remember { mutableStateOf(false) }
 
   LaunchedEffect(Unit) {
@@ -91,6 +100,7 @@ fun Explore(nav: NavigationActions, eventViewModel: EventViewModel) {
         eventList.addAll(allEvents)
       }
 
+      // wait for user input
       while (locationPermitted.value == null) {
         delay(100)
       }
@@ -115,6 +125,19 @@ fun Explore(nav: NavigationActions, eventViewModel: EventViewModel) {
   }
 
   Scaffold(
+      floatingActionButton = {
+        if (locationPermitted.value == true) {
+          FloatingActionButton(
+              onClick = { moveToCurrentLocation.value = true },
+              modifier = Modifier.size(45.dp),
+              containerColor = DarkCyan) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.location_icon),
+                    contentDescription = null,
+                    tint = Color.White)
+              }
+        }
+      },
       bottomBar = {
         BottomNavigationMenu(
             onTabSelect = { selectedTab ->
@@ -143,7 +166,6 @@ fun Explore(nav: NavigationActions, eventViewModel: EventViewModel) {
       }
 }
 
-@SuppressLint("MissingPermission")
 @Composable
 fun GoogleMapView(
     modifier: Modifier = Modifier,
@@ -154,49 +176,68 @@ fun GoogleMapView(
     query: MutableState<String>,
     locationPermitted: Boolean
 ) {
-  val locations = events.map { event -> LatLng(event.location.latitude, event.location.longitude) }
-  val states = locations.map { location -> rememberMarkerState(position = location) }
+  val coroutineScope = rememberCoroutineScope()
+
+  val eventLocations =
+      events.map { event -> LatLng(event.location.latitude, event.location.longitude) }
+  val eventStates = eventLocations.map { location -> rememberMarkerState(position = location) }
+
   val uiSettings by remember {
-    mutableStateOf(MapUiSettings(compassEnabled = false, zoomControlsEnabled = false))
+    mutableStateOf(
+        MapUiSettings(
+            compassEnabled = false, zoomControlsEnabled = false, myLocationButtonEnabled = false))
   }
   val mapProperties by remember {
     mutableStateOf(MapProperties(mapType = MapType.NORMAL, isMyLocationEnabled = locationPermitted))
   }
   val mapVisible by remember { mutableStateOf(true) }
+  val cameraPositionState =
+      CameraPositionState(position = CameraPosition.fromLatLngZoom(currentPosition, deafaultZoom))
+
+  LaunchedEffect(moveToCurrentLocation.value) {
+    coroutineScope.launch {
+      cameraPositionState.animate(
+          update =
+              CameraUpdateFactory.newCameraPosition(
+                  CameraPosition.fromLatLngZoom(currentPosition, deafaultZoom)),
+          durationMs = 1000)
+      moveToCurrentLocation.value = false
+    }
+  }
 
   if (mapVisible) {
-    GoogleMap(
-        modifier = modifier,
-        cameraPositionState =
-            CameraPositionState(
-                position = CameraPosition.fromLatLngZoom(currentPosition, deafaultZoom)),
-        properties = mapProperties,
-        uiSettings = uiSettings,
-        onMapLoaded = onMapLoaded,
-        onPOIClick = {}) {
-          val markerClick: (Marker) -> Boolean = { false }
+    Box(Modifier.fillMaxSize()) {
+      GoogleMap(
+          modifier = modifier,
+          cameraPositionState = cameraPositionState,
+          properties = mapProperties,
+          uiSettings = uiSettings,
+          onMapLoaded = onMapLoaded,
+          onPOIClick = {}) {
+            val markerClick: (Marker) -> Boolean = { false }
 
-          for (i in states.indices) {
-            MarkerInfoWindowContent(
-                state = states[i],
-                title = events[i].title,
-                icon =
-                    BitmapDescriptorFactory.defaultMarker(
-                        BitmapDescriptorFactory.HUE_RED), // TODO: change this
-                onClick = markerClick,
-                visible =
-                    events[i]
-                        .title
-                        .contains(
-                            query.value,
-                            ignoreCase =
-                                true) // maybe it would also make sense to be able to search for
-                // creators or tags ?
-                ) {
-                  Text(it.title!!, color = Color.Black)
-                }
+            for (i in eventStates.indices) {
+              MarkerInfoWindowContent(
+                  state = eventStates[i],
+                  title = events[i].title,
+                  icon =
+                      BitmapDescriptorFactory.defaultMarker(
+                          BitmapDescriptorFactory.HUE_RED), // TODO: change this
+                  onClick = markerClick,
+                  visible =
+                      events[i]
+                          .title
+                          .contains(
+                              query.value,
+                              ignoreCase =
+                                  true) // maybe it would also make sense to be able to search for
+                  // creators or tags ?
+                  ) {
+                    Text(it.title!!, color = Color.Black)
+                  }
+            }
+            content()
           }
-          content()
-        }
+    }
   }
 }
