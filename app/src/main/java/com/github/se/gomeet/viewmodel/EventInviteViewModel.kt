37cf6446c99d3881b4d1fbfea9b13fb191461da8
chorfi <chorfiyouss@gmail.com>
2024-04-23 22:58:26 +0200

    changed DB to Repository

--- app/src/main/java/com/github/se/gomeet/viewmodel/EventInviteViewModel.kt
+++ app/src/main/java/com/github/se/gomeet/viewmodel/EventInviteViewModel.kt
@@ -11,12 +11,12 @@ import kotlinx.coroutines.CompletableDeferred
 // _db argument to be able to pass a mock FirebaseFirestore instance for testing
 class EventInviteViewModel {
 
-    private val db = InvitesRepository(Firebase.firestore)
+    private val repository = InvitesRepository(Firebase.firestore)
 
     suspend fun getUsersInvitedToEvent(eventId: String): UserInvitedToEvents? {
         return try {
             val userInvitedToEvents = CompletableDeferred<UserInvitedToEvents?>()
-            db.getEventInvites(eventId) { t -> userInvitedToEvents.complete(t) }
+            repository.getEventInvites(eventId) { t -> userInvitedToEvents.complete(t) }
             userInvitedToEvents.await()
         } catch (e: Exception) {
             null
@@ -27,7 +27,7 @@ class EventInviteViewModel {
     suspend fun getEventsUserHasBeenInvitedTo(userId: String): EventInviteUsers? {
         return try {
             val eventInviteUsers = CompletableDeferred<EventInviteUsers?>()
-            db.getUserInvites(userId) { t -> eventInviteUsers.complete(t) }
+            repository.getUserInvites(userId) { t -> eventInviteUsers.complete(t) }
             eventInviteUsers.await()
         } catch (e: Exception) {
             null
@@ -37,22 +37,22 @@ class EventInviteViewModel {
     /*Return true if invite has been successfully sent*/
     fun sendInviteToUser(userID: String, eventId: String): Boolean? {
         return try {
-            db.sendInvite(userID, eventId)
+            repository.sendInvite(userID, eventId)
         } catch (e: Exception) {
             null
         }
     }
 
     fun userAcceptsInvite(userId: String, eventId: String){
-        db.udpateInvite(userId, eventId, InviteStatus.ACCEPTED)
+        repository.udpateInvite(userId, eventId, InviteStatus.ACCEPTED)
     }
 
     fun userRefusesInvite(userId: String, eventId: String){
-        db.udpateInvite(userId, eventId, InviteStatus.REFUSED)
+        repository.udpateInvite(userId, eventId, InviteStatus.REFUSED)
     }
 
     fun removeInvite(userId: String, eventId: String){
-        db.removeInvite(eventId, userId)
+        repository.removeInvite(eventId, userId)
     }
 
 }
