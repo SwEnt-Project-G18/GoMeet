febaa96b78938fa985486bc7435ed00171c5b3d7
Antoine Garin <antoine.garin@epfl.ch>
2024-04-24 10:40:43 +0200

    ktfmt format

--- app/src/main/java/com/github/se/gomeet/ui/mainscreens/events/EventInvites.kt
+++ app/src/main/java/com/github/se/gomeet/ui/mainscreens/events/EventInvites.kt
@@ -4,16 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.se.gomeet.viewmodel.EventInviteViewModel
 
-@Composable
-fun EventInvites(viewModel: EventInviteViewModel) {
-
-}
-
-
-
+@Composable fun EventInvites(viewModel: EventInviteViewModel) {}
 
 @Preview
 @Composable
 fun PreviewEventInvites() {
-    EventInvites(EventInviteViewModel())
+  EventInvites(EventInviteViewModel())
 }
--- app/src/main/java/com/github/se/gomeet/viewmodel/EventInviteViewModel.kt
+++ app/src/main/java/com/github/se/gomeet/viewmodel/EventInviteViewModel.kt
@@ -11,48 +11,46 @@ import kotlinx.coroutines.CompletableDeferred
 // _db argument to be able to pass a mock FirebaseFirestore instance for testing
 class EventInviteViewModel {
 
-    private val repository = InvitesRepository(Firebase.firestore)
+  private val repository = InvitesRepository(Firebase.firestore)
 
-    suspend fun getUsersInvitedToEvent(eventId: String): UserInvitedToEvents? {
-        return try {
-            val userInvitedToEvents = CompletableDeferred<UserInvitedToEvents?>()
-            repository.getEventInvites(eventId) { t -> userInvitedToEvents.complete(t) }
-            userInvitedToEvents.await()
-        } catch (e: Exception) {
-            null
-        }
+  suspend fun getUsersInvitedToEvent(eventId: String): UserInvitedToEvents? {
+    return try {
+      val userInvitedToEvents = CompletableDeferred<UserInvitedToEvents?>()
+      repository.getEventInvites(eventId) { t -> userInvitedToEvents.complete(t) }
+      userInvitedToEvents.await()
+    } catch (e: Exception) {
+      null
     }
+  }
 
-
-    suspend fun getEventsUserHasBeenInvitedTo(userId: String): EventInviteUsers? {
-        return try {
-            val eventInviteUsers = CompletableDeferred<EventInviteUsers?>()
-            repository.getUserInvites(userId) { t -> eventInviteUsers.complete(t) }
-            eventInviteUsers.await()
-        } catch (e: Exception) {
-            null
-        }
+  suspend fun getEventsUserHasBeenInvitedTo(userId: String): EventInviteUsers? {
+    return try {
+      val eventInviteUsers = CompletableDeferred<EventInviteUsers?>()
+      repository.getUserInvites(userId) { t -> eventInviteUsers.complete(t) }
+      eventInviteUsers.await()
+    } catch (e: Exception) {
+      null
     }
+  }
 
-    /*Return true if invite has been successfully sent*/
-    fun sendInviteToUser(userID: String, eventId: String): Boolean? {
-        return try {
-            repository.sendInvite(userID, eventId)
-        } catch (e: Exception) {
-            null
-        }
+  /*Return true if invite has been successfully sent*/
+  fun sendInviteToUser(userID: String, eventId: String): Boolean? {
+    return try {
+      repository.sendInvite(userID, eventId)
+    } catch (e: Exception) {
+      null
     }
+  }
 
-    fun userAcceptsInvite(userId: String, eventId: String){
-        repository.udpateInvite(userId, eventId, InviteStatus.ACCEPTED)
-    }
+  fun userAcceptsInvite(userId: String, eventId: String) {
+    repository.udpateInvite(userId, eventId, InviteStatus.ACCEPTED)
+  }
 
-    fun userRefusesInvite(userId: String, eventId: String){
-        repository.udpateInvite(userId, eventId, InviteStatus.REFUSED)
-    }
+  fun userRefusesInvite(userId: String, eventId: String) {
+    repository.udpateInvite(userId, eventId, InviteStatus.REFUSED)
+  }
 
-    fun removeInvite(userId: String, eventId: String){
-        repository.removeInvite(eventId, userId)
-    }
-
+  fun removeInvite(userId: String, eventId: String) {
+    repository.removeInvite(eventId, userId)
+  }
 }
