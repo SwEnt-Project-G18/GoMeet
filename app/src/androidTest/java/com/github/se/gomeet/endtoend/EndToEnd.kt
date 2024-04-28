8ca303c739bdb01f650bbbbc9f0fa81320f8d7b9
DanThePol <daniel@thepolkadots.net>
2024-04-28 10:03:20 +0200

    Updated EndToEnd test and EventsTest

--- app/src/androidTest/java/com/github/se/gomeet/endtoend/EndToEnd.kt
+++ app/src/androidTest/java/com/github/se/gomeet/endtoend/EndToEnd.kt
@@ -12,6 +12,7 @@ import com.github.se.gomeet.screens.WelcomeScreenScreen
 import com.github.se.gomeet.viewmodel.EventViewModel
 import com.github.se.gomeet.viewmodel.UserViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -25,7 +26,9 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.AfterClass
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -38,29 +41,6 @@ class EndToEndTest : TestCase() {
 
   @get:Rule val mockkRule = MockKRule(this)
 
-  private lateinit var userVM: UserViewModel
-  private lateinit var eventVM: EventViewModel
-
-  private val email = "qwe@asd.com"
-  private val pwd = "123456"
-  private val uid = "testuid"
-  private val username = "testuser"
-
-@Before
-fun setup() {
-  Firebase.auth.useEmulator("10.0.2.2", 9099)
-  Firebase.firestore.useEmulator("10.0.2.2", 8080)
-  Firebase.storage.useEmulator("10.0.2.2", 9199)
-
-  userVM = UserViewModel()
-  userVM.createUserIfNew(uid, username)
-  Firebase.auth.createUserWithEmailAndPassword(email, pwd)
-  TimeUnit.SECONDS.sleep(2)
-
-  eventVM = EventViewModel(uid)
-  println("eventVM created")
-}
-
   @After
   fun tearDown() {
 
@@ -72,11 +52,9 @@ fun setup() {
     }
 
     // Clean up the user
-    userVM.deleteUser("testuid")
     Firebase.auth.currentUser?.delete()
+    userVM.deleteUser("testuid")
   }
-
-
 
   @Test
   fun test() = run {
@@ -111,46 +89,34 @@ fun setup() {
 
     composeTestRule.waitForIdle()
 
-    ComposeScreen.onComposeScreen<CreateScreen>(composeTestRule) {
-      step("goTo publicCreate") {
-        createPublicEventButton {
-          assertIsDisplayed()
-          performClick()
-        }
-      }
-    }
 
-    ComposeScreen.onComposeScreen<CreateEventScreen>(composeTestRule) {
-      step("add event") {
-        title {
-          assertIsDisplayed()
-          performTextInput("Title")
-        }
-        description {
-          assertIsDisplayed()
-          performTextInput("Description")
-        }
-        location {
-          assertIsDisplayed()
-          performTextInput("Lausanne")
-        }
-        date {
-          assertIsDisplayed()
-          performTextInput("2003-01-01")
-        }
-        price {
-          assertIsDisplayed()
-          performTextInput("0.0")
-        }
-        link {
-          assertIsDisplayed()
-          performTextInput("https://example.com")
-        }
-        postButton {
-          assertIsDisplayed()
-          performClick()
-        }
-      }
+  }
+
+
+  companion object {
+
+    private val email = "qwe@asd.com"
+    private val pwd = "123456"
+    private val uid = "testuid"
+    private val username = "testuser"
+
+    private lateinit var userVM: UserViewModel
+    private lateinit var eventVM: EventViewModel
+    @JvmStatic
+    @BeforeClass
+    fun setup() {
+      Firebase.auth.useEmulator("10.0.2.2", 9099)
+      Firebase.firestore.useEmulator("10.0.2.2", 8080)
+      Firebase.storage.useEmulator("10.0.2.2", 9199)
+
+      userVM = UserViewModel()
+      userVM.createUserIfNew(uid, username)
+      Firebase.auth.createUserWithEmailAndPassword(email, pwd)
+      TimeUnit.SECONDS.sleep(2)
+
+      eventVM = EventViewModel(uid)
     }
   }
 }
+
+
--- app/src/androidTest/java/com/github/se/gomeet/ui/mainscreens/EventsTest.kt
+++ app/src/androidTest/java/com/github/se/gomeet/ui/mainscreens/EventsTest.kt
@@ -1,14 +1,21 @@
 package com.github.se.gomeet.ui.mainscreens
 
+import android.net.Uri
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.navigation.compose.rememberNavController
 import com.github.se.gomeet.model.event.location.Location
 import com.github.se.gomeet.ui.navigation.NavigationActions
 import com.github.se.gomeet.viewmodel.EventViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import java.time.LocalDate
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 
@@ -69,4 +76,13 @@ class EventsTest {
       Events(nav = NavigationActions(rememberNavController()), eventViewModel = EventViewModel())
     }
   }
+
+    companion object {
+        @JvmStatic
+        @BeforeClass
+        fun setup(): Unit {
+            Firebase.firestore.useEmulator("10.0.2.2", 8080)
+            Firebase.storage.useEmulator("10.0.2.2", 9199)
+        }
+    }
 }
