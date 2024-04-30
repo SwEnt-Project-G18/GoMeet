97d0b504a6b206015cbbbaf4f8ea8c57d3bc84f9
Erik <125990940+koma2323@users.noreply.github.com>
2024-04-30 21:17:57 +0200

    ktfmtFormat

--- app/src/androidTest/java/com/github/se/gomeet/screens/TrendsScreen.kt
+++ app/src/androidTest/java/com/github/se/gomeet/screens/TrendsScreen.kt
@@ -1,11 +1,9 @@
 package com.github.se.gomeet.screens
 
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
-import com.github.se.gomeet.ui.navigation.Route
 import io.github.kakaocup.compose.node.element.ComposeScreen
-import io.github.kakaocup.compose.node.element.KNode
 
 class TrendsScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ComposeScreen<TrendsScreen>(
-        semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("TrendsScreen") }) {
-}
+        semanticsProvider = semanticsProvider,
+        viewBuilderAction = { hasTestTag("TrendsScreen") }) {}
