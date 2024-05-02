6603e1be209af7f7cb47a5dada9f6fafaca2a2de
Erik <125990940+koma2323@users.noreply.github.com>
2024-05-02 19:57:05 +0200

    fixed EditProfileTest

--- app/src/androidTest/java/com/github/se/gomeet/ui/mainscreens/profile/EditProfileTest.kt
+++ app/src/androidTest/java/com/github/se/gomeet/ui/mainscreens/profile/EditProfileTest.kt
@@ -6,6 +6,12 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.compose.rememberNavController
 import com.github.se.gomeet.ui.navigation.NavigationActions
+import com.github.se.gomeet.viewmodel.UserViewModel
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 
@@ -26,5 +32,36 @@ class EditProfileTest {
     composeTestRule.onNodeWithText("Phone Number").assertIsDisplayed()
     composeTestRule.onNodeWithText("Country").assertIsDisplayed()
     composeTestRule.onNodeWithText("Edit Tags").assertIsDisplayed()
+  }
+
+  companion object {
+    private val userViewModel = UserViewModel()
+    private lateinit var currentUserId: String
+
+    private val usr = "u@t.com"
+    private val pwd = "123456"
+
+    @BeforeClass
+    @JvmStatic
+    fun setUp() {
+      Firebase.auth.createUserWithEmailAndPassword(usr, pwd)
+      TimeUnit.SECONDS.sleep(2)
+      Firebase.auth.signInWithEmailAndPassword(usr, pwd)
+      TimeUnit.SECONDS.sleep(2)
+      // Set up the user view model
+      // Order is important here, since createUserIfNew sets current user to created user (so we
+      // need to create the current user last)
+      currentUserId = Firebase.auth.currentUser!!.uid
+      userViewModel.createUserIfNew(currentUserId, "a", "b", "c", usr, "4567", "Angola")
+      TimeUnit.SECONDS.sleep(2)
+    }
+
+    @AfterClass
+    @JvmStatic
+    fun tearDown() {
+      // Clean up the user view model
+      Firebase.auth.currentUser!!.delete()
+      userViewModel.deleteUser(currentUserId)
+    }
   }
 }
