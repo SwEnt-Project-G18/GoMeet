d6ea452b662c4d3efd4eb9eae6d5a3f7e7e242cf
koma2323 <kyykyy11111@gmail.com>
2024-05-15 10:48:52 +0200

    explorescreentest

--- app/src/main/java/com/github/se/gomeet/ui/mainscreens/Explore.kt
+++ app/src/main/java/com/github/se/gomeet/ui/mainscreens/Explore.kt
@@ -223,7 +223,7 @@ fun Explore(nav: NavigationActions, eventViewModel: EventViewModel) {
                   frontLayerScrimColor = Color.Unspecified,
                   headerHeight = halfHeight.dp,
                   peekHeight = 0.dp,
-                  modifier = Modifier.testTag("ExploreScreen").padding(innerPadding),
+                  modifier = Modifier.testTag("MapSlider").padding(innerPadding),
                   appBar = {},
                   frontLayerContent = {
                     Column(modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)) {
