a728f09a57087c5ddfd1d130b76cd1c7686ddb91
Antoine Garin <antoine.garin@epfl.ch>
2024-05-16 10:47:48 +0200

    notifications screen test ktfmt format

--- app/src/androidTest/java/com/github/se/gomeet/ui/mainscreens/profile/NotificationsTest.kt
+++ app/src/androidTest/java/com/github/se/gomeet/ui/mainscreens/profile/NotificationsTest.kt
@@ -1,7 +1,6 @@
 package com.github.se.gomeet.ui.mainscreens.profile
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
