a2089a8362506852be797262c51b529ffeb1d771
Antoine Garin <antoine.garin@epfl.ch>
2024-05-16 10:36:38 +0200

    fixed Notifications screen tests

--- app/src/androidTest/java/com/github/se/gomeet/ui/mainscreens/profile/NotificationsTest.kt
+++ app/src/androidTest/java/com/github/se/gomeet/ui/mainscreens/profile/NotificationsTest.kt
@@ -3,7 +3,9 @@ package com.github.se.gomeet.ui.mainscreens.profile
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.compose.rememberNavController
 import com.github.se.gomeet.ui.navigation.NavigationActions
 import org.junit.Rule
@@ -18,11 +20,10 @@ class NotificationsTest {
     // Test the Notifications screen
     composeTestRule.setContent { Notifications(NavigationActions(rememberNavController()), "1234") }
 
-    composeTestRule.onNodeWithTag("TopBar").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("Back").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("AllButton").assertIsDisplayed().assertIsEnabled()
-    composeTestRule.onNodeWithTag("InvitationsButton").assertIsDisplayed().assertIsEnabled()
-    composeTestRule.onNodeWithTag("MyEventsButton").assertIsDisplayed().assertIsEnabled()
+    composeTestRule.onNodeWithTag("NotificationsScreen").assertExists()
+    composeTestRule.onNodeWithText("Invitations").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Messages").assertIsDisplayed()
+    composeTestRule.onNodeWithContentDescription("Go Back").assertIsDisplayed()
     //    composeTestRule.onNodeWithTag("InviterUserName").assertIsDisplayed()
     //    composeTestRule.onNodeWithTag("EventCard").assertIsDisplayed()
     //    composeTestRule.onNodeWithTag("EventDate").assertIsDisplayed()
