9b5ee17bc2a37a0e0615a1a184b2b79337f9cd41
koma2323 <kyykyy11111@gmail.com>
2024-05-17 07:51:41 +0200

    test

---
+++ app/src/androidTest/java/com/github/se/gomeet/screens/ManageInvitesScreen.kt
@@ -0,0 +1,9 @@
+package com.github.se.gomeet.screens
+
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import io.github.kakaocup.compose.node.element.ComposeScreen
+
+class ManageInvitesScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<ManageInvitesScreen>(
+        semanticsProvider = semanticsProvider,
+        viewBuilderAction = { hasTestTag("ManageInvitesScreen") }) {}
