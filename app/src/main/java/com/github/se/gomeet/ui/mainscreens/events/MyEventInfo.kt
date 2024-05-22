67bc0db15d6e7d82528d12f2dde58093a086719b
koma2323 <kyykyy11111@gmail.com>
2024-05-22 20:54:30 +0200

    changed map style in eventinfo

--- app/src/main/java/com/github/se/gomeet/ui/mainscreens/events/MyEventInfo.kt
+++ app/src/main/java/com/github/se/gomeet/ui/mainscreens/events/MyEventInfo.kt
@@ -1,6 +1,7 @@
 package com.github.se.gomeet.ui.mainscreens.events
 
 import android.util.Log
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -29,8 +30,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+import com.github.se.gomeet.R
 import com.github.se.gomeet.model.event.Event
 import com.github.se.gomeet.model.user.GoMeetUser
 import com.github.se.gomeet.ui.mainscreens.LoadingText
@@ -40,9 +43,12 @@ import com.github.se.gomeet.viewmodel.UserViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
@@ -173,8 +179,19 @@ private fun MapViewComposable(
     loc: LatLng,
     zoomLevel: Float = 15f // Default zoom level for close-up of location
 ) {
+  val ctx = LocalContext.current
+  val isDarkTheme = isSystemInDarkTheme()
+
   val cameraPositionState = rememberCameraPositionState {
     position = CameraPosition.fromLatLngZoom(loc, zoomLevel)
+  }
+  val mapProperties by remember {
+    mutableStateOf(
+        MapProperties(
+            mapType = MapType.NORMAL,
+            mapStyleOptions =
+                MapStyleOptions.loadRawResourceStyle(
+                    ctx, if (isDarkTheme) R.raw.map_style_dark else R.raw.map_style_light)))
   }
 
   val markerState = rememberMarkerState(position = loc)
@@ -183,7 +200,8 @@ private fun MapViewComposable(
   GoogleMap(
       modifier =
           Modifier.testTag("MapView").fillMaxWidth().height(200.dp).clip(RoundedCornerShape(20.dp)),
-      cameraPositionState = cameraPositionState) {
+      cameraPositionState = cameraPositionState,
+      properties = mapProperties) {
         Marker(
             state = markerState,
             title = "Marker in Location",
